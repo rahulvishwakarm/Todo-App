fc9195f04cb134118b56214c0cf5e674cb3ba7e6
Rahul Vishwakarma <init830@gmail.com>
2020-10-06 16:14:58 +0530

    todo app

---
+++ backend/backend/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include                 # add this
+from rest_framework import routers                    # add this
+from todo import views                            # add this
+        
+router = routers.DefaultRouter()                      # add this
+router.register(r'todos', views.TodoView, 'todo')     # add this
+        
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', admin.site.urls)         
+                   # add this
+]
